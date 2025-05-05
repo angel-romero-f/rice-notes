b7c4895e83c1603eb9243b957e332b579d33643b
angel-romero-f <angelfloromero@gmail.com>
2025-05-05 02:21:11 -0400

    Setup routing boilerplate using chi and make simple test route

--- backend/internal/routes/routes.go
+++ backend/internal/routes/routes.go
@@ -1,1 +1,25 @@
+// Package routes provides the router to set up handlers for the incoming HTTP requests.
 package routes
+
+import (
+	"net/http"
+
+	internal_middleware "github.com/angel-romero-f/rice-notes/internal/middleware"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+func NewRouter() *chi.Mux {
+	r := chi.NewRouter()
+
+	// Middlewares
+	r.Use(middleware.Logger)
+	r.Use(internal_middleware.CORSMiddleware)
+
+	// Routes
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Response"))
+	})
+
+	return r
+}
